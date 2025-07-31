0dae94bde774aedc616b6cee2428a10b93ad8392
Ahmadkalims <ahmadkalims@yahoo.com>
2025-07-31 15:25:21 +0530

    Arrow

--- 2. Patterns/9.Pattern_diamond.cpp
+++ 2. Patterns/9.Pattern_diamond.cpp
@@ -2,20 +2,22 @@
 using namespace std;
 
 int main() {
-    int n = 5;
+    int n;
+    cout << "Enter the length: ";
+    cin >> n;
 
+    // Upper part of the arrow
     for (int i = 0; i < n; i++) {
         for (int k = n - i - 2; k >= 0; k--) {
             cout << "  ";
         }
-
         for (int j = 0; j < 2 * i + 1; j++) {
             cout << " *";
         }
-
         cout << endl;
     }
 
+    // Lower part of the arrow
     for (int i = 1; i < n; i++) {
         for (int j = 1; j <= i; j++) {
             cout << "  ";
@@ -23,7 +25,6 @@ int main() {
         for (int j = 0; j < (2 * n) - (2 * i) - 1; j++) {
             cout << " *";
         }
-
         cout << endl;
     }
 
